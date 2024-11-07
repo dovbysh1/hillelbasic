fa7708e60dfbe72c5163288f2fda082498b55127
dovbysh1 <ydovbyshenko@gmail.com>
2024-11-07 17:30:20 +0200

    commit

---
+++ src/ydovbyshenko/homework4.java
@@ -0,0 +1,28 @@
+package ydovbyshenko;
+
+public class homework4 {
+    public static void main(String[] args) {
+        byte number = 1;
+        String productName = "smartphone";
+        double productTotalSales = 12153.41;
+        int productDays = 5;
+
+        double productDailySales = productTotalSales / productDays;
+
+        System.out.println("Product No " + number+ ": " + productName + ",");
+        System.out.println("total sales for " + productDays + " days is EUR " + productTotalSales + ",");
+        System.out.printf("sales by day is EUR " + productDailySales + ".\n");
+
+        number++;
+        productName = "laptop";
+        productTotalSales = 10486.85;
+        productDays = 7;
+
+        productDailySales = productTotalSales / productDays;
+
+        System.out.println("Product No " + number+ ": " + productName + ",");
+        System.out.println("total sales for " + productDays + " days is EUR " + productTotalSales + ",");
+        System.out.printf("sales by day is EUR " + productDailySales + ".");
+
+    }
+}
