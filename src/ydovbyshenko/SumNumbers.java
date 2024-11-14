c6fbcb3c0be3daf6436807ad8986131988ed95fe
dovbysh1 <ydovbyshenko@gmail.com>
2024-11-14 19:33:27 +0200

    #6.1 SumNumbers

--- src/ydovbyshenko/SumNumbers.java
+++ src/ydovbyshenko/SumNumbers.java
@@ -1,10 +1,16 @@
 package ydovbyshenko;
 
+import java.util.Scanner;
+
 public class SumNumbers {
     public static void main(String[] args) {
+        System.out.print("Enter bound: ");
+        Scanner scanner = new Scanner(System.in);
+                int bound = scanner.nextInt();
+
         int sum = 0;
 
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= bound; i++) {
             sum += i;
             System.out.printf("%d) Num is %d, sum is %d%n", i, i, sum);
         }
